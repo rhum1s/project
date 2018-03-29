dc581d2989b4bf1eb1faed42e15298b5f793c606
rhum1s <romain.souweine@hotmail.fr>
2018-03-29 21:17:14 +0200

    MAin screen init

---
+++ game_vars.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3.6
+"""
+Can change during game
+"""
+
+import pygame
+from global_vars import *
+
+# Sprites lists
+all_sprites_list = pygame.sprite.Group()
+
+# Player default start position
+PLAYER_START_POS_X = (SCREEN_WIDTH / 2) - 35
+PLAYER_START_POS_Y = SCREEN_HEIGHT - (70 * 2)
+
+# Player characteristics
+PLAYER_STARTING_LIVES = 3 
+
+# Game conditions
+LEVEL = 1
+LOOSE = False
+
---
+++ global_vars.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.6
+"""
+Will not be modified during game.
+"""
+
+# Screen definition
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (100, 255, 0)
+YELLOW = (255, 255, 0)
+BLUE = (0, 0, 255)
+LIGHT_BLUE = (102, 204, 255)
+LIGHT_GREY = (200, 200, 200)
